c802eb4f6bea52e89775ed885a02376689c2e769
CharlesKJunior <mutebwacharles@gmail.com>
2025-04-18 20:51:00 +0300

    my first class

---
+++ class Pet:.py
@@ -0,0 +1,14 @@
+class Pet:
+    def __init__(self, name):
+        """
+        Initialize a new pet with a name, and default attributes:
+        hunger: 5 (moderately hungry)
+        energy: 5 (moderately rested)
+        happiness: 5 (moderately happy)
+        tricks: empty list for learned tricks
+        """
+        self.name = name
+        self.hunger = 5
+        self.energy = 5
+        self.happiness = 5
+        self.tricks = []  # for bonus
